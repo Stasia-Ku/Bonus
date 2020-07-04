a1dd17a83456288b56334d7e4862361d8c4d7ba8
Stasia-Ku <67242445+Stasia-Ku@users.noreply.github.com>
2020-07-04 15:00:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         float currientValue = 100.0f;
-        float additionaltValue = 1100.0f;
+        float additionaltValue = 1200.0f;
         int bonusValue = 100;
+        int bonusResult =0;
 
-        int bonusResult = (int) additionaltValue / bonusValue;
+        if (additionaltValue > 1000) {
+            bonusResult = (int) additionaltValue / bonusValue;
+        }
 
         float resultValue = currientValue + additionaltValue + bonusResult;
 
